dd2eb98374892a447e6c9487f66a046acf02f0c4
Kim Jihoon <max.kim@ebridge-world.com>
2022-07-13 17:41:46 +0900

    init

---
+++ variables.ts
@@ -0,0 +1,9 @@
+let apples: number = 5;
+let speed: string = 'fast';
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3, 4, 5];
+let truths: boolean[] = [true, true, false];
