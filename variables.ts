5616b1c8ea26d9a3de4881687697d5a3f247febd
Kim Jihoon <max.kim@ebridge-world.com>
2022-07-15 16:48:20 +0900

    type inference

--- variables.ts
+++ variables.ts
@@ -1,4 +1,4 @@
-let apples: number = 5;
+let apples = 5;
 let speed: string = 'fast';
 
 let nothingMuch: null = null;
@@ -7,3 +7,49 @@ let nothing: undefined = undefined;
 let colors: string[] = ['red', 'green', 'blue'];
 let myNumbers: number[] = [1, 2, 3, 4, 5];
 let truths: boolean[] = [true, true, false];
+
+class Car {
+
+}
+
+let car: Car = new Car();
+
+// Object literal
+let point: { x: number; y: number; } = {
+    x: 10,
+    y: 20
+};
+
+// Function
+const logNumber = (i: number): void => {
+    console.log(i);
+};
+
+// When to use annotations
+// 1) Function that returns the 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates: { x: number; y: number; } = JSON.parse(json);
+console.log(coordinates.x);
+
+// 2) When we declare a variable on one line
+// and initialize it later
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+    console.log(words[i])
+    if (words[i] === 'green') {
+        foundWord = true;
+    }
+}
+
+// 3) Variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 0) {
+        numberAboveZero = numbers[i]
+    }
+}
+console.log(`numberAboveZero ${numberAboveZero}`);
